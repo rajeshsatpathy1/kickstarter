a62e5128e42187a135a6c541d2935f9bddb9ce94
rajeshsatpathy1 <rajesh.a.s.1997@gmail.com>
2023-09-27 21:16:09 -0500

    Fix the sepolia issue and add next.js libraries
    - Add getInitialProps
    - Update CampaignFactory address
    - Update gitignore to include nextjs

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x90e2b94532734E2999D4919Ea1b5263Bf35d4B0d'
+  '0x6a9e0AAa3Bb67973108D7CbAb24Ad6320D8E8d03'
 );
 
 export default instance;
