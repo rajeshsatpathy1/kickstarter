c5a6f69857e346a217808d1191bd6fff67038e59
rajeshsatpathy1 <rajesh.a.s.1997@gmail.com>
2023-09-27 20:21:57 -0500

    Add index page, web3.js and factory.js
    - Rename campaign page to index page
    - Get Deployed campaigns from it
    - Access metamask provider using Alchemy API key
    - Make the Campaign Factory accessible by passing instance of its contract at factory.js

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -3,9 +3,9 @@ const { Web3 } = require('web3');
 const compiledFactory = require('./build/CampaignFactory.json');
 
 const provider = new HDWalletProvider(
-    'diet snow neck yellow urban fashion omit above term robot enemy cactus',
+  'diet snow neck yellow urban fashion omit above term robot enemy cactus',
   // remember to change this to your own phrase!
-  'https://eth-sepolia.g.alchemy.com/v2/VLvHCP0vCSgBezMWUiijnTv5_ruucnGl'
+  'https://eth-sepolia.g.alchemy.com/v2/oredQhe8Za30oMGRVwdrreWxMo1ibSJv'
   // remember to change this to your own endpoint!
 );
 const web3 = new Web3(provider);
