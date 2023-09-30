42c700383cb2cdd455c154f0eb8381708915c532
rajeshsatpathy1 <rajesh.a.s.1997@gmail.com>
2023-09-29 21:00:05 -0500

    install next packages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4617a8f3adab0b6f"; }
+/******/ 		__webpack_require__.h = function() { return "a74ec8ddc87c7566"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
