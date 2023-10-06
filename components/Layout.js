35234bce1d9b7f4b2af6a86357ec3cf589cba643
rajeshsatpathy1 <rajesh.a.s.1997@gmail.com>
2023-10-05 19:49:02 -0500

    Create a page for creating campaigns
    - Update the layout to have the stylesheet, remove it from index.js
    - Delete unused show page
    - The new page uses forms and accesses metamask
    - A way to handle errors on wrong inputs and reject needs to be checked

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,19 @@
 import { Container } from "semantic-ui-react";
 import React from "react";
+import Head from "next/head";
 import Header from "./Header";
 
 const Layout = (props) => {
     return (
         <div>
             <Container>
+                <Head>
+                <link
+                    async
+                    rel="stylesheet"
+                    href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+                />
+                </Head>
                 <Header />
                 {props.children}
             </Container>
