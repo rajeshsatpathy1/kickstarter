0f8ba20dcb64978d1f125b394c913c5c28f6361c
rajeshsatpathy1 <rajesh.a.s.1997@gmail.com>
2023-10-09 19:18:08 -0500

    Add routes for new campaign, update header and index
    - ALso add a Show campaign page to the campaigns components

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Menu } from "semantic-ui-react";
+import { Link } from "../routes";
 
 const Header = () => {
     return (
-        <Menu>
-            <Menu.Item>CrowdCoin</Menu.Item>
+        <Menu style={{ marginTop: "10px" }}>
+            <Link route="/">
+                <a className="item">CrowdCoin</a>
+            </Link>
             
             <Menu.Menu position="right">
-                <Menu.Item>Campaign</Menu.Item>
-                <Menu.Item>+</Menu.Item>
+                <Link route="/">
+                    <a className="item">Campaigns</a>
+                </Link>
+                <Link route="/campaigns/new">
+                    <a className="item">+</a>
+                </Link>
             </Menu.Menu>
         </Menu>
     )
