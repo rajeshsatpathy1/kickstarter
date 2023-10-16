5a2d69a6cdf15e5a6e7c8a76de9a0fcd4e6c7746
rajeshsatpathy1 <rajesh.a.s.1997@gmail.com>
2023-10-16 00:27:29 -0500

    Update contract to add a getSummary attribute and update address

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x6a9e0AAa3Bb67973108D7CbAb24Ad6320D8E8d03'
+  '0xb1d000dE5e4CF49A05851b55bf14b8643D042844'
 );
 
 export default instance;
