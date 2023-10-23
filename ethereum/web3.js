cbd6b0bfb6947b9521f07890d85f8394d204382e
rajeshsatpathy1 <rajesh.a.s.1997@gmail.com>
2023-10-22 23:33:03 -0500

    Add refresh on contribute form submit and try apikey provider in web3.js

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -8,8 +8,8 @@ if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
   web3 = new Web3(window.ethereum);
 } else {
   // We are on the server *OR* the user is not running metamask
-  const provider = new Web3.providers.HttpProvider(
-    'https://eth-sepolia.g.alchemy.com/v2/MdYMalbYuBp8eoUID4jsG7MijqwEICqX'
+  const provider = new Web3.providers.WebsocketProvider(
+    'wss://eth-sepolia.g.alchemy.com/v2/MdYMalbYuBp8eoUID4jsG7MijqwEICqX'
   );
   web3 = new Web3(provider);
 }
