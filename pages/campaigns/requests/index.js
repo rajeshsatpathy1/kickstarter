e5954b0329be1e3057adb1f817dec960a0815cce
rajeshsatpathy1 <rajesh.a.s.1997@gmail.com>
2023-11-08 21:05:47 -0600

    Add RequestRow logic and add its changes in requests index

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -3,33 +3,50 @@ import { Button, Table } from "semantic-ui-react";
 import { Link } from "../../../routes";
 import Layout from "../../../components/Layout";
 import Campaign from "../../../ethereum/campaign";
+import RequestRow from "../../../components/RequestRow";
 
 class RequestIndex extends Component {
     static async getInitialProps(props) {
         const { address } = props.query;
         const campaign = Campaign(address);
         const requestCount = await campaign.methods.getRequestsCount().call();
-        Array(parseInt(requestCount));
+        const approversCount = Number(await campaign.methods.approversCount().call());
 
         const requests = await Promise.all(
-            Array(requestCount)
+            Array(parseInt(requestCount))
                 .fill()
                 .map((element, index) => {
                     return campaign.methods.requests(index).call();
                 })
         );
 
-        return { address, requests, requestCount };
+        return { address, requests, requestCount, approversCount };
+    }
+
+    renderRows() {
+        return this.props.requests.map((request, index) => {
+            {console.log("abcd")}
+            return (
+                <RequestRow
+                    key={index}
+                    id={index}
+                    request={request}
+                    address={this.props.address}
+                    approversCount={this.props.approversCount}
+                />
+            );
+        });
     }
 
     render() {
-        const { Header, HeaderCell, Row, Body } = Table;
+        const { Header, Row, HeaderCell, Body } = Table;
+
         return (
             <Layout>
-                <h3> Requests </h3>
+                <h3>Requests</h3>
                 <Link route={`/campaigns/${this.props.address}/requests/new`}>
                     <a>
-                        <Button primary> Add Request </Button>
+                        <Button primary>Add Request</Button>
                     </a>
                 </Link>
                 <Table>
@@ -44,6 +61,7 @@ class RequestIndex extends Component {
                             <HeaderCell>Finalize</HeaderCell>
                         </Row>
                     </Header>
+                    <Body>{this.renderRows()}</Body>
                 </Table>
             </Layout>
         );
